b240d543a60e6d35dc5b30b4842f1ce5f6436cdc
pritom rabidas <pritomrabidas102@gmail.com>
2025-04-25 21:40:33 +0600

    login or assest file

--- account/models.py
+++ account/models.py
@@ -3,7 +3,6 @@ from django.contrib.auth.models import AbstractUser
 
 class person(AbstractUser):
     email = models.EmailField(unique=True)
-    phone = models.CharField(max_length=15, unique=True)
     address = models.TextField()
     city = models.CharField(max_length=100)
     state = models.CharField(max_length=100)
