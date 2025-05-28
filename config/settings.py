92a7882805b977738866e79cafdb991592952296
pritom rabidas <pritomrabidas102@gmail.com>
2025-05-28 12:41:39 +0600

    product image add

--- config/settings.py
+++ config/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 import dj_database_url 
 import os
-
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -79,6 +79,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
+
+DATABASES = {
+    'default': dj_database_url.config(default='sqlite:///db.sqlite3')
+}
 
 DATABASES = {
   'default': {
--- home/models.py
+++ home/models.py
@@ -7,7 +7,7 @@ class Slider(models.Model):
 
 class Catagory(models.Model):
     name = models.CharField(max_length=100)
-    
+    slug = models.SlugField(max_length=100, unique=True)
     def __str__(self):
         return self.name
 
