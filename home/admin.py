c69214f275595e8d49611b8515fec25478218f0c
pritom rabidas <pritomrabidas102@gmail.com>
2025-06-04 20:29:26 +0600

    searchProduct 1/2 complete

--- home/admin.py
+++ home/admin.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
 from .models import *
+class SearchProduct(admin.ModelAdmin):
+    list_display = ["name", "price", "brand","best_selling","discount",]
+    search_fields = ["name", "price", "brand","best_selling","discount",]
 
 admin.site.register(Slider)
 admin.site.register(Catagory)
 admin.site.register(Size)
 admin.site.register(Brand)
-admin.site.register(Product)
+admin.site.register(Product,SearchProduct)
 admin.site.register(Review)
 
 # Register your models here.
